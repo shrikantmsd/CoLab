a6600dcc29e219355db79b8e700b26a71b153a50
shrikantmsd <shrikantmsd@gmail.com>
2026-03-22 09:40:48 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    largePageDataBytes: 1024 * 1024 * 2,
-  },
-};
-
-module.exports = nextConfig;
